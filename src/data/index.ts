30b84af69b77ec91923e9be380c175fe2b29c588
OrzMiku <miku@ecy.pink>
2024-10-01 14:22:20 +0800

    init repo

---
+++ src/data/index.ts
@@ -0,0 +1,49 @@
+import type { Profile } from "../types";
+
+export const profile: Profile = {
+    name: "OrzMiku",
+    tagline: "一位来自中国的热情的编程初学者。",
+    email: "miku@ecy.pink",
+    avatar: "https://image.dooo.ng/c/2024/09/18/66eadec443e1e.webp",
+    socials: [
+        {
+            name: "GitHub",
+            url: "https://github.com/OrzMiku",
+        },
+        {
+            name: "BiliBili",
+            url: "https://space.bilibili.com/402606447",
+        },
+        {
+            name: "Blog",
+            url: "https://miku.show",
+        }
+    ],
+    projects: [
+        {
+            name: "LunarFox整合包",
+            description: "一个我的世界性能整合包，带有生活质量提升模组和 OptiFine 替代方案。该整合包包括一些开箱即用的模组配置和良好的性能配置。",
+            url: "https://modrinth.com/modpack/lunarfox",
+        },
+        {
+            name: "MinecraftShader",
+            description: "用于记录MC着色器学习。",
+            url: "https://github.com/OrzMiku/MinecraftShader",
+        },
+        {
+            name: "MinecraftSkins",
+            description: "我的世界皮肤，包括一些原创和转载的皮肤。",
+            url: "https://github.com/OrzMiku/MinecraftSkins",
+        },
+        {
+            name: "OpenGL_Learn",
+            description: "用于记录OpenGL学习。",
+            url: "https://github.com/OrzMiku/OpenGL_Learn",
+        },
+        {
+            name: "Verilog_Learn",
+            description: "用于记录Verilog_Learn学习。",
+            url: "https://github.com/OrzMiku/Verilog_Learn",
+        },
+    ],
+};
---
+++ src/types/index.ts
@@ -0,0 +1,19 @@
+export type Social = {
+    name: string;
+    url: string;
+};
+
+export type Project = {
+    name: string;
+    description: string;
+    url: string;
+};
+
+export type Profile = {
+    name: string;
+    tagline: string;
+    avatar: string;
+    email: string;
+    socials: Social[];
+    projects: Project[];
+};
