0a4685f83eca167d4943319142e9d66f10274462
OrzMiku <miku@ecy.pink>
2024-10-07 00:49:25 +0800

    Add prettier and format the code

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,23 +1,23 @@
 export type Social = {
-    name: string;
-    url: string;
+  name: string;
+  url: string;
 };
 
 export type Project = {
-    name: string;
-    description: string;
-    url: string;
+  name: string;
+  description: string;
+  url: string;
 };
 
 export type Profile = {
+  name: string;
+  tagline: string;
+  avatar: string;
+  email: string;
+  socials: Social[];
+  projects: Project[];
+  icp?: {
     name: string;
-    tagline: string;
-    avatar: string;
-    email: string;
-    socials: Social[];
-    projects: Project[];
-    icp?: {
-        name: string;
-        url: string;
-    }
+    url: string;
+  };
 };
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,17 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			container: {
-				screens: {
-					sm: '100%',
-					md: '100%',
-					lg: '1024px',
-					xl: '1280px',
-				}
-			}
-		},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      container: {
+        screens: {
+          sm: "100%",
+          md: "100%",
+          lg: "1024px",
+          xl: "1280px",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
