d83f178ae8027559ac02bff21bfd1e63a598f0aa
OrzMiku <miku@ecy.pink>
2024-11-05 10:10:40 +0800

    Update project description and links in personal profile

--- src/data/index.ts
+++ src/data/index.ts
@@ -23,7 +23,7 @@ export const profile: Profile = {
     {
       name: "LunarFox整合包",
       description:
-        "一个我的世界性能整合包，带有生活质量提升模组和 OptiFine 替代方案。该整合包包括一些开箱即用的模组配置和良好的性能配置。",
+        "一个我的世界性能整合包，带有生活质量提升模组和 OptiFine 替代方案。",
       url: "https://modrinth.com/modpack/lunarfox",
     },
     {
@@ -32,14 +32,14 @@ export const profile: Profile = {
       url: "https://github.com/OrzMiku/personal-profile",
     },
     {
-      name: "MinecraftShader",
-      description: "用于记录MC着色器学习。",
-      url: "https://github.com/OrzMiku/MinecraftShader",
+      name: "青丘文档",
+      description: "芳草遍青丘，繁花绕碧流。",
+      url: "https://qdocs.miku.show",
     },
     {
-      name: "MinecraftSkins",
-      description: "我的世界皮肤，包括一些原创和转载的皮肤。",
-      url: "https://github.com/OrzMiku/MinecraftSkins",
+      name: "像素皮肤站",
+      description: "我的世界皮肤站，不开放注册。",
+      url: "https://pixelskin.cn",
     },
     {
       name: "OpenGL_Learn",
@@ -47,9 +47,9 @@ export const profile: Profile = {
       url: "https://github.com/OrzMiku/OpenGL_Learn",
     },
     {
-      name: "Verilog_Learn",
-      description: "用于记录Verilog_Learn学习。",
-      url: "https://github.com/OrzMiku/Verilog_Learn",
+      name: "orzmiku/noname",
+      description: "无名杀Docker容器",
+      url: "https://hub.docker.com/r/orzmiku/noname",
     },
   ],
   icp: {
