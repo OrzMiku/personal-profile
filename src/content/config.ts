3ce89a41727cbda22e8a4f3e1af0ff5a55373bad
OrzMiku <miku@ecy.pink>
2024-11-09 17:55:44 +0800

    Format and Correct implicit type errors

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,18 +1,18 @@
 import { z, defineCollection } from "astro:content";
 
 const blogCollection = defineCollection({
-    type: "content",
-    schema: z.object({
-        title: z.string(),
-        date: z.date(),
-    })
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    date: z.date(),
+  }),
 });
 
 const dataCollection = defineCollection({
-    type: "data"
+  type: "data",
 });
 
 export const collections = {
-    posts: blogCollection,
-    data: dataCollection,
-}
+  posts: blogCollection,
+  data: dataCollection,
+};
